ad20ba3da9d91e9d0354fdd8b1be98f8e8e3d4b1
Arash Altafi <arashaltafi1377@gmail.com>
2025-10-08 13:18:20 +0330

    a

--- app/src/main/java/ir/arash/altafi/sample/ui/component/BackPressHandler.kt
+++ app/src/main/java/ir/arash/altafi/sample/ui/component/BackPressHandler.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import ir.arash.altafi.sample.navigation.Route
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -16,21 +18,37 @@ fun BackPressHandler(
     navController: NavController,
     onNavigationItemSelected: (Int) -> Unit
 ) {
-    var doubleBackToExitPressedOnce by remember { mutableStateOf(false) }
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination?.route
+
     val context = LocalContext.current
     val activity = (context as? Activity)
+    val packageName = context.packageName
+
+    val isHome = currentDestination == packageName + Route.Home.route
+
+    val allowBottomBar = arrayOf(
+        packageName + Route.Home.route,
+        packageName + Route.Test.route,
+        packageName + Route.Profile.route,
+        packageName + Route.Setting.route
+    )
+
+    var doubleBackToExitPressedOnce by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
 
     // Use BackHandler to intercept the system back press
     BackHandler {
-        if (navController.previousBackStackEntry != null) {
+        if (currentDestination !in allowBottomBar) {
+            navController.popBackStack()
+        } else if (navController.previousBackStackEntry != null) {
             // Pop the backstack if there is a previous route
             onNavigationItemSelected(0)
             navController.navigate(Route.Home) {
                 popUpTo(0)
                 launchSingleTop = true
             }
-        } else {
+        } else if (isHome) {
             // Handle double back press to exit the app
             if (doubleBackToExitPressedOnce) {
                 // Exit the app if back is pressed twice within 5 seconds
@@ -51,6 +69,8 @@ fun BackPressHandler(
                     doubleBackToExitPressedOnce = false
                 }
             }
+        } else {
+            navController.popBackStack()
         }
     }
 }
